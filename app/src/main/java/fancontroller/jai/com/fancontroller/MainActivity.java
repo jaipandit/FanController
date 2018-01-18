e9f2e036e3910cda597ce00fab38e5c9063c9583
Jai Pandit <jai.x.pandit.-nd@disney.com>
2018-01-18 15:14:20 -0800

    Initial commit

---
+++ app/src/main/java/fancontroller/jai/com/fancontroller/MainActivity.java
@@ -0,0 +1,16 @@
+package fancontroller.jai.com.fancontroller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jai Pandit on 1/18/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+    }
+}
