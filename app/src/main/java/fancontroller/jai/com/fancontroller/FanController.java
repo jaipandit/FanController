ec11eab2c0f115a757d28cd913301d09a6559e43
Jai Pandit <jai.x.pandit.-nd@disney.com>
2018-01-19 13:30:33 -0800

    Spinner in progress

--- app/src/main/java/fancontroller/jai/com/fancontroller/FanController.java
+++ app/src/main/java/fancontroller/jai/com/fancontroller/FanController.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
+
 
 /**
  * The fan controller custom view.
@@ -19,6 +22,9 @@ import android.view.View;
 public class FanController extends View {
 
     private static final String TAG = FanController.class.getSimpleName();
+    private static final int START_ROTATION = 1;
+    private static final int STOP_ROTATION = 2;
+    private static final long INTERVAL = 30;
 
     private float viewWidth;
     private float viewHeight;
@@ -32,6 +38,10 @@ public class FanController extends View {
     private Paint linePen;
 
     private DialStop[] stops;
+    private Handler uiHandler;
+
+
+    private boolean isRotationStarted = false;
 
     public FanController(Context context,
             @Nullable AttributeSet attrs,
@@ -89,6 +99,44 @@ public class FanController extends View {
                 return false;
             }
         });
+
+        uiHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                switch (msg.what) {
+                    case START_ROTATION:
+                        isRotationStarted = true;
+                        Log.i(TAG, "Rotation Started");
+                        rotateArrayIndexes();
+
+                        // redraw.
+                        FanController.this.invalidate();
+
+                        Log.i(TAG, "Message processed.");
+                        uiHandler.sendMessageDelayed(uiHandler.obtainMessage(START_ROTATION), INTERVAL);
+                        return true;
+                    case STOP_ROTATION:
+                        isRotationStarted = false;
+                        Log.i(TAG, "Rotation stopped");
+                        uiHandler.removeMessages(START_ROTATION);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (isRotationStarted) {
+                    uiHandler.sendMessageAtFrontOfQueue(uiHandler.obtainMessage(STOP_ROTATION));
+                } else {
+                    uiHandler.sendEmptyMessage(START_ROTATION);
+                }
+                return false;
+            }
+        });
     }
 
 
@@ -96,14 +144,24 @@ public class FanController extends View {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        Log.i(TAG, "Redrawing.");
+        /*if (dialPen.getColor() == Color.GREEN) {
+            dialPen.setColor(Color.RED);
+        } else {
+            dialPen.setColor(Color.GREEN);
+        }*/
         canvas.drawCircle(dialCenterXPosition, dialCenterYPosition, dialRadius, dialPen);
-        for (DialStop stop : stops) {
-            canvas.drawText(Integer.toString(stop.number), stop.xPos, stop.yPos, stop.pen);
+
+        for (int i = 0; i < stops.length; i++) {
+            DialStop stop = stops[i];
+            //Log.i(TAG, "Drawing number")
+            canvas.drawCircle(stop.xPos, stop.yPos, stop.radius, stop.pen);
         }
 
         // Draw margins for my help.
-        canvas.drawLine(dialCenterXPosition, 0, dialCenterXPosition, viewHeight, linePen);
-        canvas.drawLine(0, dialCenterYPosition, viewWidth, dialCenterYPosition, linePen);
+        //canvas.drawLine(dialCenterXPosition, 0, dialCenterXPosition, viewHeight, linePen);
+        //canvas.drawLine(0, dialCenterYPosition, viewWidth, dialCenterYPosition, linePen);
+
     }
 
     @Override
@@ -116,42 +174,64 @@ public class FanController extends View {
         dialCenterYPosition = viewHeight / 2;
         dialRadius = (float) (Math.min(viewWidth, viewHeight) / 2 * 0.8);
 
-        stops = createDialStops(12);
+        createDialStops(12);
     }
 
     private static class DialStop {
         private final float xPos;
         private final float yPos;
-        private final float radius;
-        private final int number;
-        private final Paint pen;
+        private float radius;
+        private final float baseRadius;
+        private int number;
+        private Paint pen;
 
-        DialStop(int number, float xPos, float yPos, float radius, Paint pen) {
+        DialStop(int number, float xPos, float yPos, float radius) {
             this.xPos = xPos;
             this.yPos = yPos;
             this.radius = radius;
-            this.number = number;
-            this.pen = pen;
+            this.baseRadius = radius;
+
+            //Create pen to draw stops.
+            pen = new Paint(Paint.ANTI_ALIAS_FLAG);
+            pen.setColor(Color.RED);
+            pen.setStyle(Paint.Style.FILL);
+
+            setNumber(number);
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(number);
+        }
+
+        public void setNumber(int color) {
+            this.number = color;
+            if (color == Color.RED) {
+                radius = radius + 20;
+            } else {
+                radius = baseRadius;
+            }
+            pen.setColor(number);
         }
     }
 
-    private DialStop[] createDialStops(int numberOfStops) {
-        DialStop[] myStops = new DialStop[numberOfStops];
+    private void createDialStops(int numberOfStops) {
+        stops = new DialStop[numberOfStops];
 
-        // we will come on this when we will study about negative angles.
         int maxAngle = 360;
         int angleOffset = maxAngle / numberOfStops;
-        int nextAngle = angleOffset;
-        for (int i = 0; i < myStops.length; i++) {
-            Pair<Double, Double> resultPair = getCoordinate(dialCenterXPosition, dialCenterYPosition, (double) dialRadius, nextAngle);
-            DialStop stop = new DialStop(i, resultPair.first.floatValue(), resultPair.second.floatValue(), 10, textPen);
-            myStops[i] = stop;
+        int nextAngle = 0;
 
+
+        for (int i = 0; i < numberOfStops; i++) {
+            Pair<Double, Double> resultPair = getCoordinate(dialCenterXPosition, dialCenterYPosition, (double) dialRadius, nextAngle);
+            DialStop stop = new DialStop(i == 0 ? Color.RED : Color.GRAY,
+                    resultPair.first.floatValue(), resultPair.second.floatValue(), 30);
+
+            stops[i] = stop;
             // prepare for next angle.
             nextAngle = nextAngle + angleOffset;
         }
-
-        return myStops;
     }
 
     private Pair<Double, Double> getCoordinate(double xPos, double yPos, double radius, double angle) {
@@ -164,4 +244,31 @@ public class FanController extends View {
 
         return new Pair<>(resultxPos, resultyPos);
     }
+
+    private void rotateArrayIndexes() {
+        int nextEntry = -1;
+        for (int i = 0; i < stops.length - 1; i++) {
+            if (nextEntry != -1) {
+                int temp = stops[i + 1].number;
+                stops[i + 1].setNumber(nextEntry);
+                nextEntry = temp;
+            } else {
+                nextEntry = stops[i + 1].number;
+                stops[i + 1].setNumber(stops[i].number);
+            }
+        }
+        stops[0].setNumber(nextEntry);
+        printArray();
+    }
+
+    private void printArray() {
+        StringBuilder sb = new StringBuilder("Data: \n");
+        for (int i = 0; i < stops.length; i++) {
+            sb.append("stop[" + i + "] ->" + stops[i].toString() + "\n");
+        }
+        sb.append("*****");
+
+        Log.i(TAG, sb.toString());
+    }
+
 }
